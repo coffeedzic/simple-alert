f23b58c76ab55b92da84b6d98da570e5fa8999c0
Edin Kahvedžić <74417220+coffeedzic@users.noreply.github.com>
2021-06-22 13:44:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function defaultSuccess() {
+    simpleAlert.success('Simple text alert');
+}
+
+function defaultError() {
+    simpleAlert.error('Simple text alert');
+}
+
+function defaultInfo() {
+    simpleAlert.info('Simple text alert');
+}
+
+function defaultWarning() {
+    simpleAlert.warning('Simple text alert');
+}
+
+function bounceSuccess() {
+    simpleAlert.success('Simple text alert', 3000, 'bounce');
+}
+
+function bounceError() {
+    simpleAlert.error('Simple text alert', 3000, 'bounce');
+}
+
+function bounceInfo() {
+    simpleAlert.info('Simple text alert', 3000, 'bounce');
+}
+
+function bounceWarning() {
+    simpleAlert.warning('Simple text alert', 3000, 'bounce');
+}
+
+function slideSuccess() {
+    simpleAlert.success('Simple text alert', 3000, 'slide');
+}
+
+function slideError() {
+    simpleAlert.error('Simple text alert', 3000, 'slide');
+}
+
+function slideInfo() {
+    simpleAlert.info('Simple text alert', 3000, 'slide');
+}
+
+function slideWarning() {
+    simpleAlert.warning('Simple text alert', 3000, 'slide');
+}
